64671126dbd35ac850f386f4cce70499f259aaaf
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-06-11 12:32:09 -0700

    add functioning-ish user input for map

--- assets/js/eatoutpage.js
+++ assets/js/eatoutpage.js
@@ -1,66 +1,40 @@
 /* VARIABLES */
-//const search = document.querySelector('.search');
+const search = document.querySelector('.search');
+const postal = document.querySelector('#location');
 
 /* FUNCTIONS */
 function initMap () {
     // Create the a map for the location
     const city = { lat: -33.866, lng: 151.196 };
-    const map = new google.maps.Map(document.querySelector(".map"), {
-        center: city,
-        zoom: 17,
-        mapId: "8d193001f940fde3",
-    });
+    const postalCode = postal.value;
+    console.log("In initMap...", postalCode);
+    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${postalCode}&key=AIzaSyAEerJOeBxyhFsP1Scr3BNLld0dITismKE`;
 
-    // Create the places service.
-    //const service = new google.maps.places.PlacesService(map);
-  
-    // Perform a nearby search.
-    // service.nearbySearch(
-    //     {location: city, radius: 500, type: "store"},
-    //     (results, status, pagination) => {
-    //         if (status !== "OK" || !results) return;
-  
-    //         addPlaces(results, map);
-    //         moreButton.disabled = !pagination || !pagination.hasNextPage;
-    //         if (pagination && pagination.hasNextPage) {
-    //             getNextPage = () => {
-    //                 // Note: nextPage will call the same handler function as the initial call
-    //                 pagination.nextPage();
-    //             };
-    //         }
-    //     },
-    // );
+    fetch(geocodeUrl)
+        .then(response => response.json())
+        .then(data => {
+        if (data.status === "OK") {
+            const city = data.results[0].geometry.location;
+            const map = new google.maps.Map(document.querySelector(".map"), {
+            center: city,
+            zoom: 17,
+            mapId: "8d193001f940fde3",
+            });
+        } else {
+            console.error("Geocode was not successful for the following reason: " + data.status);
+        }
+        })
+        .catch(error => console.error("Error fetching geocode data: ", error));
 }
 
-// function handleSearch () {
-//     console.log("Beginning API call...")
-
-//     const city = new google.maps.LatLng(-33.867, 151.195);
-
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: city,
-//         zoom: 15
-//     })
-
-//     var request = {
-//         location: city,
-//         radius: '500',
-//         type: ['restaurant']
-//     }
-
-//     service = new google.maps.places.PlacesService(map);
-//     service.nearbySearch(request, callback);
-// }
-
-// function callback(results, status) {
-//     if (status == google.maps.places.PlacesServiceStatus.OK) {
-//         for (var i = 0; i < results.length; i++) {
-//         createMarker(results[i]);
-//         }
-//     }
-// }
+function handleSearch () {
+    const postal = document.querySelector('#location');
+    console.log("In handle request...", postal)
+    initMap()
+    postal.value = "";
+}
 
 /* EVENT LISTENERS */
-//search.addEventListener('click', handleSearch)
+search.addEventListener('click', handleSearch)
 
 window.initMap = initMap;
