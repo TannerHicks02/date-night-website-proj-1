caceb1214ee78ac6a121a8c6565f74dce11352e2
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-13 11:58:01 -0400

    hitting the modal submit button now stores zipcode in localStorage and redirects to eatout-html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,8 @@
 // DEPENDENCIES
 const eatInButton = document.querySelector("#eatIn-button");
 const eatOutButton = document.querySelector("#eatOut-button");
+const zipInput = document.querySelector('#zip-input');
+const submitButton = document.querySelector('#submit-button');
 
 console.log("scripts loaded");
 
@@ -12,5 +14,21 @@ eatOutButton.addEventListener('click', function (event) {
 
 eatInButton.addEventListener('click', function (event) {
     event.preventDefault();
-    window.location.href = "eatin.html";
+    // window.location.href = "eatin.html";
+});
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let zipCode = zipInput.value;
+
+    if (zipCode === '') {
+        displayMessage('error', 'Enter ZIP Code.');
+    }
+    else {
+        localStorage.setItem('zipCode', zipCode);
+    }
+
+    console.log(localStorage);
+    window.location.href = "eatout.html";
 });
