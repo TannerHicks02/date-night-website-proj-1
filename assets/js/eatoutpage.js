ba6b2cb8b862b0f6969526627cffe3f478690dfe
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-06-13 09:25:43 -0700

    add initialization to the right zipcode

--- assets/js/eatoutpage.js
+++ assets/js/eatoutpage.js
@@ -1,54 +1,35 @@
 /* VARIABLES */
 const search = document.querySelector('.search');
-const postal = Number(localStorage.getItem('zipcode')) || 58504
+const postal = Number(localStorage.getItem('zipcode')) || 58504;
 const mapDiv = document.querySelector(".map");
 const restaurants = document.querySelector("#restaurant-cards")
 const APIKey = "AIzaSyDJLuOe9XH4mw4Kal20XkEmhwlGDNhRsYE";
+let latlong = { lat: -33.866, lng: 151.196 };
 let map;
 
+// map.setCenter(lat long)
+
 /* FUNCTIONS */
-/* Get map of input zipcode */
+/* Find restaurants near location according to user input */
 function getInputMap() {
-    const cuisine = document.querySelector('#cuisine');
-    console.log(cuisine)
-    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${postal}&key=${APIKey}`;
+    // const cuisine = document.querySelector('#cuisine').value;
 
-    // Call to Geocoder API
-    fetch(geocodeUrl)
-        .then(response => response.json())
-        .then(data => {
-        if (data.status === "OK") {
-            const location = data.results[0].geometry.location;  // latitude/longitude coordinates
+    const request = {
+        location: latlong,
+        radius: document.querySelector('#radius').value,
+        type: ['restaurant'],
+        //keyword: 'thai'
+    }
 
-            map = new google.maps.Map(mapDiv, {  // create map using lat/long coordinates
-                center: location,
-                zoom: 17,
-                mapId: "8d193001f940fde3",
-            });
-
-            findRestaurants(location);  // Get restaurants on map
-        } else {
-            console.error("Geocode was not successful for the following reason: " + data.status);
-        }
-        })
-        .catch(error => console.error("Error fetching geocode data: ", error));
-}
-
-/* Find restaurants near location */
-function findRestaurants (location) {
-    const request = {  // Parameters for restaurants
-        location: location,
-        radius: '16000',
-        type: ['restaurant']
-    };
-
+    console.log(request)
+    
     const service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, (results, status) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (let i = 0; i < results.length && i < 10; i++) {
-            createMarker(results[i]);
-            createCard(results[i]);
-        }
+            for (let i = 0; i < results.length && i < 10; i++) {
+                createMarker(results[i]);
+                createCard(results[i]);
+            }
         } else {
             console.error("Places service was not successful for the following reason: " + status);
         }
@@ -61,38 +42,24 @@ function createCard(place) {
 
     // Create
     const card = document.createElement("div");
-    const cardBody = document.createElement('div');
-    const cardTitle = document.createElement('h5');
+    const cardTitle = document.createElement('h3');
     const open = document.createElement("p");
     const rating = document.createElement("p");
     const location = document.createElement("p");
 
     // Build
-    // card.classList.add('rest-cards');
-    card.classList.add("card")
-    card.setAttribute('style','width: 18rem');
-
-    cardBody.classList.add('card-body');
-
-    cardTitle.classList.add('card-title');
+    card.classList.add('rest-cards');
     cardTitle.textContent = place.name;
-
-    open.classList.add('card-text');
-    open.textContent = isOpen(place.opening_hours.open_now);
-
-    rating.classList.add('card-text');
+    // open.textContent = isOpen(place.opening_hours.open_now);
     rating.textContent =  "⭐" + place.rating;
-
-    location.classList.add('card-text');
     location.textContent = place.vicinity;
 
     // Place
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(open);
-    cardBody.appendChild(rating);
-    cardBody.appendChild(location);
-    card.appendChild(cardBody);
     restaurants.appendChild(card);
+    card.appendChild(cardTitle);
+    // card.appendChild(open);
+    card.appendChild(rating);
+    card.appendChild(location);
 }
 
 /* Add markers on map for restaurants */
@@ -123,13 +90,48 @@ function isOpen (open) {
 search.addEventListener('click', getInputMap)
 
 /* INITIALIZERS */
-/* Initiialize default map */
+/* Initiialize default map and all nearby restaurants*/
 window.initMap = initMap;
 function initMap () {
-    const temp = { lat: -33.866, lng: 151.196 };  // Defaults to Metcalfe Park, Sydney, Australia 
-    map = new google.maps.Map(mapDiv, {
-        zoom: 18,
-        center: temp,
-        mapId: "DEFAULT_MAP",
+    const defaultUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${postal}&key=${APIKey}`;
+    fetch(defaultUrl)
+        .then(response => response.json())
+        .then(data => {
+        if (data.status === "OK") {
+            latlong = data.results[0].geometry.location;  // Set latitude/longitude coordinates
+
+            map = new google.maps.Map(mapDiv, {  // Create map using lat/long coordinates
+                center: latlong,
+                zoom: 10,
+                mapId: "8d193001f940fde3",
+            });
+    
+            findAllRestaurants();  // Get restaurants on map
+        } else {
+            console.error("Geocode was not successful: " + data.status);
+        }
+    })
+    .catch(error => console.error("Error fetching geocode data: ", error));
+}
+
+function findAllRestaurants() {
+
+    const request = {
+        location: latlong,
+        radius: '8046',
+        type: ['restaurant']
+    }
+
+    console.log(request)
+    const service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (let i = 0; i < results.length && i < 5; i++) {
+                createMarker(results[i]);
+                createCard(results[i]);
+            }
+        } else {
+            console.error("Places service was not successful: " + status);
+        }
     });
 }
