c600138b4544475dda774560525506017e87ae63
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-14 12:54:28 -0400

    created modal for night-out button to redirect html page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,8 +2,11 @@
 const nightOutButton = document.querySelector("#nightOut-button");
 const eatOutButton = document.querySelector("#eatOut-button");
 const zipInput = document.querySelector('#zip-input');
+const zipInput2 = document.querySelector('#zip-input2');
 const submitButton = document.querySelector('#zipcode-submit-button');
+const submitButton2 = document.querySelector('#zipcode-submit-button2');
 const warning = document.querySelector('#warning');
+const warning2 = document.querySelector('#warning2');
 
 // EVENT LISTENERS
 eatOutButton.addEventListener('click', function (event) {
@@ -13,7 +16,7 @@ eatOutButton.addEventListener('click', function (event) {
 
 nightOutButton.addEventListener('click', function (event) {
     event.preventDefault();
-    window.location.href = "nightout.html";
+    // window.location.href = "nightout.html";
 });
 
 submitButton.addEventListener('click', function (event) {
@@ -28,5 +31,20 @@ submitButton.addEventListener('click', function (event) {
     else {
         localStorage.setItem('zipCode', zipCode);
         window.location.href = "eatout.html";
+    }
+});
+
+submitButton2.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let zipCode2 = zipInput2.value;
+
+    if (zipCode2 === '') {
+        //displayMessage('error', 'Please enter a ZIP Code.');
+        warning2.textContent = "Please enter a Zipcode"
+    }
+    else {
+        localStorage.setItem('zipCode', zipCode2);
+        window.location.href = "nightout.html";
     }
 });
